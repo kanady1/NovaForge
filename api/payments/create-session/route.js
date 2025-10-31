10ee808d03a3b2df936497cf952530d13d912d01
kanady1 <kanady-10@hotmail.com>
2025-10-31 16:06:25 +0200

    Delete api directory

--- api/payments/create-session/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { createCheckout } from "../../../../lib/payments.js";
-
-export async function POST(req){
-  const { provider, planId, email } = await req.json();
-  // TODO: persist intent in DB (userId/email/plan)
-  const session = await createCheckout({ provider, planId, userId: email||'guest', email });
-  return NextResponse.json(session);
-}
